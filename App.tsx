2f7082614df4fb10b87e819ab73bb14878f89143
Boda0000 <abdelrahmankamel812@gmail.com>
2025-10-12 15:37:11 +0300

    Test

--- App.tsx
+++ App.tsx
@@ -1,23 +1,20 @@
 import React from "react";
 import Navigator from "./src/screen/Navigation/Navigator";
 import reactotron from "reactotron-react-native";
-import './ReactotronConfig';
+import "./ReactotronConfig";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import Toast from "react-native-toast-message";
 
-
-if (__DEV__) {
-  import('./ReactotronConfig');
-}
-
 // @ts-ignore
-console.tron.log('🚀 Reactotron Connected!');
+console.tron.log("🚀 Reactotron Connected!");
 
 const queryClient = new QueryClient();
 
 export default function App() {
-  return (<QueryClientProvider client={queryClient}>
+  return (
+    <QueryClientProvider client={queryClient}>
       <Navigator />
       <Toast />
-    </QueryClientProvider>);
+    </QueryClientProvider>
+  );
 }

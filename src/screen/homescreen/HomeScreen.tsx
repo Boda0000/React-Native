33721939b50cdec0e643e52a327d88929a69ffb1
Boda0000 <abdelrahmankamel812@gmail.com>
2025-11-11 09:42:01 +0200

    handle i18n , style in same page

--- src/screen/homescreen/HomeScreen.tsx
+++ src/screen/homescreen/HomeScreen.tsx
@@ -1,7 +1,7 @@
-import { View, Text, FlatList } from "react-native";
-import styles from "./styles";
+import { View, Text, FlatList, StyleSheet, Platform } from "react-native";
 import SAR from "../../assets/icons/SAR.svg";
 import Calender from "../../assets/icons/Calender.svg";
+import i18n from "src/locales/i18n";
 
 type Order = {
   id: string;
@@ -17,45 +17,48 @@ const orders: Order[] = [
     ordernumber: "6526851",
     price: 29,
     date: "24/12/2024",
-    status: "تم الاستلام",
+    status: i18n.t("Received"),
   },
   {
     id: "2",
     ordernumber: "6526851",
     price: 29,
     date: "24/12/2024",
-    status: "تم الاستلام",
+    status: i18n.t("Received"),
   },
   {
     id: "3",
     ordernumber: "6526851",
     price: 29,
     date: "24/12/2024",
-    status: "تم الاستلام",
+    status: i18n.t("Received"),
   },
   {
     id: "4",
     ordernumber: "6526851",
     price: 18,
     date: "23/12/2024",
-    status: "ملغي",
+    status: i18n.t("Cancelled"),
   },
   {
     id: "5",
     ordernumber: "6526851",
     price: 18,
     date: "23/12/2024",
-    status: "ملغي",
+    status: i18n.t("Cancelled"),
   },
 ];
 
 const OrderCard = ({ item }: { item: Order }) => {
-  const isReceived = item.status === "تم الاستلام";
+  const isReceived = item.status === i18n.t("Received");
 
   return (
     <View style={styles.card1}>
       <View style={styles.h1}>
-        <Text style={styles.orderNumber}>رقم الطلب #{item.ordernumber}</Text>
+        <Text style={styles.orderNumber}>
+          {i18n.t("ordernumber")}
+          {item.ordernumber}
+        </Text>
 
         <View style={styles.priceContainer}>
           <Text style={styles.price}>{item.price}</Text>
@@ -67,7 +70,8 @@ const OrderCard = ({ item }: { item: Order }) => {
         <Text
           style={[
             styles.statusText,
-            { color: isReceived ? "#00A86B" : "#D1003F" },
+            { color: isReceived ? "#289CA5" : "#CE0043" },
+            { borderColor: isReceived ? "#289CA5" : "#CE0043" },
           ]}
         >
           {item.status}
@@ -83,7 +87,7 @@ const OrderCard = ({ item }: { item: Order }) => {
 };
 
 const OrdersScreen = () => {
- return (
+  return (
     <View style={styles.container1}>
       <FlatList
         data={orders}
@@ -92,6 +96,93 @@ const OrdersScreen = () => {
       />
     </View>
   );
-}
+};
+
+const styles = StyleSheet.create({
+  container1: {
+    flex: 1,
+    backgroundColor: "#f4f6f8",
+    padding: 15,
+    paddingTop: Platform.OS === "android" ? 50 : 80,
+  },
+
+  card1: {
+    backgroundColor: "#DCF1F3",
+    borderRadius: 14,
+    marginHorizontal: 9,
+    marginBottom: 20,
+    paddingHorizontal: 15,
+    paddingVertical: 20,
+    height: 100,
+    borderWidth: 1,
+    borderColor: "#BEDBDE",
+
+    shadowColor: "#DCEBEC",
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 1,
+    shadowRadius: 4,
+
+    elevation: 3,
+  },
+
+  h1: {
+    flexDirection: "row-reverse",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+
+  h2: {
+    flexDirection: "row-reverse",
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginTop: 8,
+  },
+
+  orderNumber: {
+    textAlign: "right",
+    fontSize: 16,
+    color: "#385052",
+    marginTop: 2,
+    fontWeight: "500",
+    fontFamily: "IBMPlexSansArabic-Medium",
+  },
+
+  priceContainer: {
+    flexDirection: "row-reverse",
+    alignItems: "center",
+  },
+
+  price: {
+    color: "#289CA5",
+    fontWeight: "700",
+    fontSize: 14,
+    textAlign: "right",
+    fontFamily: "IBM Plex Sans Arabic",
+  },
+
+  dateContainer: {
+    flexDirection: "row-reverse",
+    alignItems: "center",
+    gap: 4,
+  },
+
+  date: {
+    fontSize: 12,
+    fontWeight: "400",
+    fontFamily: "IBMPlexSansArabic-Medium",
+    color: "#385052",
+  },
+
+  statusText: {
+    fontWeight: "700",
+    fontSize: 14,
+    borderWidth: 1,
+    borderColor: "#289CA5",
+    borderRadius: 8,
+    fontFamily: "IBMPlexSansArabic-Bold",
+    backgroundColor: "#FFFFFF66",
+    padding: 7,
+  },
+});
 
 export default OrdersScreen;

c8251865483a720123d680468a057713eb493f0e
Boda0000 <abdelrahmankamel812@gmail.com>
2025-11-25 14:03:37 +0200

    quiz screen

--- src/screen/homescreen/HomeScreen.tsx
+++ src/screen/homescreen/HomeScreen.tsx
@@ -1,260 +1,193 @@
+import { useState } from "react";
 import {
   View,
   Text,
-  FlatList,
+  Image,
   StyleSheet,
-  Platform,
+  TextInput,
   TouchableOpacity,
+  ScrollView,
+  Platform,
+  FlatList,
 } from "react-native";
-import SAR from "../../assets/icons/SAR.svg";
-import Calender from "../../assets/icons/Calender.svg";
-import i18n from "src/locales/i18n";
-import { colors } from "../../assets/colors/colors";
-import CustomButton from "../../components/btn/CustomButton";
-import { useNavigation } from "@react-navigation/native";
-import SidebarModal from "src/components/modal/SidebarModal";
-import { Ionicons } from "@expo/vector-icons";
-import { useState } from "react";
+import { colors } from "src/assets/colors/colors";
+import Quiz from "../../assets/icons/quiz.svg";
+import Leftarrow from "../../assets/icons/leftarrow.svg";
+import Rightarrow from "../../assets/icons/rightarrow.svg";
 
-type Order = {
-  id: string;
-  ordernumber: string;
-  price: number;
-  date: string;
-  status: string;
-};
+export default function Counter() {
+  const numbers = [1, 2, 3, 4, 5, 6, 7, 8];
 
-const orders: Order[] = [
-  {
-    id: "1",
-    ordernumber: "6526851",
-    price: 29,
-    date: "24/12/2024",
-    status: i18n.t("Received"),
-  },
-  {
-    id: "2",
-    ordernumber: "6526851",
-    price: 29,
-    date: "24/12/2024",
-    status: i18n.t("Received"),
-  },
-  {
-    id: "3",
-    ordernumber: "6526851",
-    price: 29,
-    date: "24/12/2024",
-    status: i18n.t("Received"),
-  },
-  {
-    id: "4",
-    ordernumber: "6526851",
-    price: 18,
-    date: "23/12/2024",
-    status: i18n.t("Cancelled"),
-  },
-  {
-    id: "5",
-    ordernumber: "6526851",
-    price: 18,
-    date: "23/12/2024",
-    status: i18n.t("Cancelled"),
-  },
-];
+  const [currentnumber, setcurrentnumber] = useState(0);
+  const [completed, setCompleted] = useState<number[]>([]);
+  const [wrong, setWrong] = useState<number[]>([]);
 
-const OrdersScreen = () => {
-  const navigation = useNavigation<any>();
-  const [sidebarVisible, setSidebarVisible] = useState(false);
+  function onPressNumber(index: number) {
+    if (!completed.includes(currentnumber)) {
+      setCompleted((prev) => [...prev, currentnumber]);
+    }
 
-  const OrderCard = ({ item }: { item: Order }) => {
-    const isReceived = item.status === i18n.t("Received");
-
-    return (
-      <View style={styles.card1}>
-        <View style={styles.h1}>
-          <Text style={styles.orderNumber}>
-            {i18n.t("ordernumber")}
-            {item.ordernumber}
-          </Text>
-
-          <View style={styles.priceContainer}>
-            <Text style={styles.price}>{item.price}</Text>
-            <SAR width={20} height={17} />
-          </View>
-        </View>
-
-        <View style={styles.h2}>
-          <Text
-            style={[
-              styles.statusText,
-              { color: isReceived ? colors.primary500 : colors.error500 },
-              {
-                borderColor: isReceived ? colors.primary500 : colors.error500,
-              },
-            ]}
-          >
-            {item.status}
-          </Text>
-
-          <View style={styles.dateContainer}>
-            <Calender width={13} height={13} />
-            <Text style={styles.date}>{item.date}</Text>
-          </View>
-        </View>
-      </View>
-    );
-  };
+    if (!completed && !wrong.includes(currentnumber)) {
+      setWrong((prev) => [...prev, currentnumber]);
+    }
+    setcurrentnumber(index);
+  }
 
   return (
-    <View style={styles.container1}>
-      <TouchableOpacity
-        onPress={() => setSidebarVisible(true)}
-        style={{
-          position: "absolute",
-          top: Platform.OS === "android" ? 55 : 90,
-          right: 20,
-          zIndex: 10,
-        }}
-      >
-        <Ionicons name="menu" size={28} color={colors.neutral800} />
-      </TouchableOpacity>
+    <ScrollView contentContainerStyle={styles.container}>
+      <View style={{ marginVertical: 20 }}>
+        <FlatList
+          data={numbers}
+          horizontal
+          contentContainerStyle={styles.counterRow}
+          renderItem={({ item, index }) => (
+            <TouchableOpacity
+              onPress={() => onPressNumber(index)}
+              style={[
+                styles.numbers,
+                completed.includes(index) && styles.correct,
+                wrong.includes(index) && styles.wrong,
+                index === currentnumber && styles.current,
+              ]}
+            >
+              <Text style={styles.circleText}>{item}</Text>
+            </TouchableOpacity>
+          )}
+        />
+      </View>
 
-      <FlatList
-        data={orders}
-        keyExtractor={(item) => item.id}
-        renderItem={({ item }) => <OrderCard item={item} />}
-        contentContainerStyle={{
-          paddingTop: Platform.OS === "android" ? 100 : 60,
-        }}
+      <Text style={styles.questionNumber}>السؤال 4 من 8 (مقالي)</Text>
+      <View style={{ flexDirection: "row", justifyContent: "center", gap: 11 }}>
+        <Text style={styles.questionText}>اكتب عن كيفية نمو الأسماك</Text>
+        <Quiz width={22} height={22} />
+      </View>
+
+      <Image
+        style={styles.image}
+        source={require("../../assets/images/quiz.png")}
       />
 
-      {/* <View style={styles.footer}>
-        <CustomButton
-          title={i18n.t("Next")}
-          onPress={() => navigation.navigate("CartScreen")}
-          buttonStyle={styles.nextButton}
-          textStyle={styles.nextButtonText}
-        />
-        <CustomButton
-          title={i18n.t("Next")}
-          onPress={() => navigation.navigate("ProductsScreen")}
-          buttonStyle={[styles.nextButton, { marginTop: 10 }]}
-          textStyle={styles.nextButtonText}
-        />
-      </View> */}
-
-      <SidebarModal
-        visible={sidebarVisible}
-        onClose={() => setSidebarVisible(false)}
+      <TextInput
+        style={styles.input}
+        placeholder="اكتب اجابتك هنا..."
+        placeholderTextColor="#6C798D"
       />
-    </View>
+
+      <View style={styles.buttonsRow}>
+        <TouchableOpacity style={styles.button}>
+          <Leftarrow width={15} height={15} />
+          <Text style={styles.buttonText}>السؤال التالي</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.buttonText}>السؤال السابق</Text>
+          <Rightarrow width={15} height={15} />
+        </TouchableOpacity>
+      </View>
+    </ScrollView>
   );
-};
+}
 
 const styles = StyleSheet.create({
-  container1: {
+  container: {
     flex: 1,
-    backgroundColor: colors.white,
-    padding: 15,
-    paddingTop: Platform.OS === "android" ? 50 : 80,
+    padding: 16,
+    backgroundColor: colors.background,
+    paddingTop: Platform.OS === "android" ? 50 : 120,
   },
 
-  card1: {
-    backgroundColor: colors.cardLight,
-    borderRadius: 14,
-    marginHorizontal: 9,
-    marginBottom: 20,
-    paddingHorizontal: 15,
-    paddingVertical: 20,
-    height: 100,
+  counterRow: {
+    flexDirection: "row-reverse",
+    justifyContent: "center",
+    marginBottom: 15,
+    flexWrap: "wrap",
+    gap: 9,
+  },
+
+  numbers: {
+    width: 35,
+    height: 35,
+    borderRadius: 10,
+    justifyContent: "center",
+    alignItems: "center",
     borderWidth: 1,
-    borderColor: colors.borderLight,
-
-    shadowColor: colors.shadow,
-    shadowOffset: { width: 0, height: 2 },
-    shadowOpacity: 1,
-    shadowRadius: 4,
-
-    elevation: 3,
   },
 
-  h1: {
-    flexDirection: "row-reverse",
-    justifyContent: "space-between",
-    alignItems: "center",
+  circleText: {
+    color: "#000",
   },
 
-  h2: {
-    flexDirection: "row-reverse",
-    justifyContent: "space-between",
-    alignItems: "center",
-    marginTop: 8,
+  current: {
+    backgroundColor: "#082375",
+    borderColor: "#082375",
   },
 
-  orderNumber: {
-    textAlign: "right",
+  correct: {
+    backgroundColor: "#00CE3A1A",
+    borderColor: "#00CE3A",
+  },
+
+  wrong: {
+    backgroundColor: "#CE00431A",
+    borderColor: "#CE0043",
+  },
+
+  questionNumber: {
+    textAlign: "center",
+    color: "#52383C",
+    marginBottom: 20,
+    fontWeight: 400,
     fontSize: 16,
-    color: colors.text,
-    marginTop: 2,
-    fontWeight: "500",
-    fontFamily: "IBMPlexSansArabic-Medium",
-  },
-
-  priceContainer: {
-    flexDirection: "row-reverse",
-    alignItems: "center",
-  },
-
-  price: {
-    color: colors.primary500,
-    fontWeight: "700",
-    fontSize: 14,
-    textAlign: "right",
     fontFamily: "IBM Plex Sans Arabic",
   },
 
-  dateContainer: {
-    flexDirection: "row-reverse",
-    alignItems: "center",
-    gap: 4,
-  },
-
-  date: {
-    fontSize: 12,
-    fontWeight: "400",
-    fontFamily: "IBMPlexSansArabic-Medium",
-    color: colors.text,
-  },
-
-  statusText: {
-    fontWeight: "700",
-    fontSize: 14,
-    borderWidth: 1,
-    borderRadius: 8,
+  questionText: {
+    fontSize: 18,
+    fontWeight: 700,
     fontFamily: "IBMPlexSansArabic-Bold",
-    backgroundColor: colors.neutral150,
-    padding: 7,
+    textAlign: "center",
+    marginBottom: 15,
+    color: "#52383C",
   },
 
-  // footer: {
-  //   position: "absolute",
-  //   bottom: 15,
-  //   left: 15,
-  //   right: 15,
-  // },
+  image: {
+    width: "100%",
+    height: 200,
+    borderRadius: 20,
+    marginBottom: 20,
+  },
 
-  // nextButton: {
-  //   backgroundColor: "#315C63",
-  //   width: "100%",
-  //   borderRadius: 4,
-  //   height: 50,
-  // },
-  // nextButtonText: {
-  //   fontSize: 18,
-  //   fontWeight: "500",
-  //   color: "#fff",
-  //   fontFamily: "Kalligraaf Arabic Medium",
-  // },
+  input: {
+    height: 130,
+    borderWidth: 1,
+    borderColor: "#DAE0F0",
+    borderRadius: 14,
+    padding: 16,
+    marginBottom: 25,
+  },
+
+  buttonsRow: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+
+  button: {
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    backgroundColor: "#FFFFFF",
+    borderRadius: 14,
+    borderWidth: 1,
+    borderColor: "#082375",
+    justifyContent: "center",
+    flexDirection: "row",
+    gap: 2,
+    alignItems: "center",
+  },
+
+  buttonText: {
+    color: "#082375",
+    fontWeight: 700,
+    fontSize:14,
+    fontFamily:"IBMPlexSansArabic-Bold",
+  },
 });
-
-export default OrdersScreen;
